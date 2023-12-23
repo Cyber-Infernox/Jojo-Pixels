ee40978a611abefc883720e0a154588a64dbd18f
Cyber_Infernox <sayonkar@gmail.com>
2023-12-23 21:34:16 +0530

    Fixes

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -2,8 +2,8 @@ import Gallery from "@/components/shared/Gallery/Gallery";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="head-text text-left">Home</h1>
+    <main className="">
+      <h1 className="text-center font-bold mb-3">Photos</h1>
       <Gallery />
     </main>
   );
