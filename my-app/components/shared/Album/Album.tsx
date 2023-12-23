32a3c6258358dcbefb6e9a9e18cf95e78e834c3b
Cyber_Infernox <sayonkar@gmail.com>
2023-12-23 23:22:51 +0530

    Profile layout

--- my-app/components/shared/Album/Album.tsx
+++ my-app/components/shared/Album/Album.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-// import CloseIcon from "@mui/icons-material/Close";
-// import OpenInFullIcon from "@mui/icons-material/OpenInFull";
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import OpenInFullIcon from "@mui/icons-material/OpenInFull";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import Image from "next/image";
 import { useState } from "react";
 import "./Album.css";
@@ -22,7 +21,7 @@ const Album = ({ photo }: any) => {
           alt="close"
           width={18}
           height={18}
-          className="cursor-pointer object-contain bg-white"
+          className="closure"
           onClick={() => setModel(false)}
         />
         <Image src="/uploads/cover.jpeg" alt="logo" width={900} height={900} />
@@ -50,23 +49,7 @@ const Album = ({ photo }: any) => {
                   : "hidden"
               }
             >
-              <Image
-                src="/assets/close.svg"
-                alt="close"
-                width={18}
-                height={18}
-                className="cursor-pointer object-contain"
-                onClick={() => setModel(true)}
-              />
-              <Image
-                src="/assets/close.svg"
-                alt="close"
-                width={18}
-                height={18}
-                className="cursor-pointer object-contain"
-                //   onClick={handleDelete}
-              />
-              {/* <OpenInFullIcon
+              <OpenInFullIcon
                 className="mr-14"
                 sx={{ color: "white" }}
                 onClick={() => setModel(true)}
@@ -75,16 +58,11 @@ const Album = ({ photo }: any) => {
                 className="mr-14"
                 sx={{ color: "white" }}
                 // onClick={handleDelete}
-              /> */}
-              <Image
-                src="/assets/delete.svg"
-                alt="delete"
-                width={18}
-                height={18}
-                className="cursor-pointer object-contain"
-                //   onClick={handleDelete}
               />
-              {/* <DeleteIcon sx={{ color: "white" }} onClick={handleDelete} /> */}
+              <DeleteIcon
+                sx={{ color: "white" }}
+                // onClick={handleDelete}
+              />
             </div>
           </div>
         </div>
--- my-app/components/shared/Profile/Profile.tsx
+++ my-app/components/shared/Profile/Profile.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import "./Profile.css";
+import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 const Profile = () => {
+  const [followed, setFollowed] = useState(false);
+
   return (
     <div className="profileRight">
       <div className="profileRightTop">
@@ -24,6 +31,26 @@ const Profile = () => {
         <div className="profileInfo">
           <h4 className="profileInfoName">Mrinal Kar</h4>
           <span className="profileInfoDesc">Capturing all the way...</span>
+          <span className="profileInfoLoc">Kolkata, India</span>
+          <button className="rightbarFollowButton">
+            {followed ? (
+              <div className="rightbarFollowText">Unfollow</div>
+            ) : (
+              <div className="rightbarFollowText">Follow</div>
+            )}
+            {followed ? <RemoveIcon /> : <AddIcon />}
+          </button>
+          <div className="profileInfoLoc">
+            <span className="mr-[20px]">
+              10 - <b>Followers</b>
+            </span>
+            <span className="mr-[20px]">
+              10 - <b>Photo Likes</b>
+            </span>
+            <span className="mr-[20px]">
+              10 - <b>Followings</b>
+            </span>
+          </div>
         </div>
       </div>
       {/* <div className="profileRightBottom">
